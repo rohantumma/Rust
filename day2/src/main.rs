be9285d9e8775ffedea2036f18f1020635416394
Rohan Tumma <Rohan.TUMMA@3ds.com>
2024-01-12 08:34:30 +0530

    loops

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -11,10 +11,28 @@ fn main() {
 
     // conditioanl assignment
 
-    let mut loki = if (x != 10) {100} else {-100};
+    let mut loki = if x != 10 {100} else {-100};
 
     println!("loki={}", loki);
 
-    // Loops for while etc
+    // Loops with break 
+
+    let mut count : i32 = 0;
+    loop {
+        if count == 10{
+            break;
+        }
+        println!("{}", count);
+        count +=1;
+    }
+
+    //for loop
+
+    let mut ARR = [1,2,3,4,5];
+    for pat in ARR {
+        println!("{}", pat);
+    }
+
+
 
 }
