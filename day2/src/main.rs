72c7b70a91fecded1fa90e321c96b5bd93a6c223
Rohan Tumma <Rohan.TUMMA@3ds.com>
2024-01-14 10:36:59 +0530

    Structs and generic's

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,9 +1,16 @@
+use std::fs;
+
 fn main() {
     println!("Hello, world!");
     // strings_here();
     // owner_ship();
     // rustModules();
-    rustcrates();
+    // rustcrates();
+    // clArg();
+    // fileRead();
+    // fileWrite();
+    // structs_here();
+    gneric_struct();
 }
 
 fn loops_here(){
@@ -115,7 +122,7 @@ fn rustcrates(){
     // add dependancy to toml file
     use rand;
 
-    let number = rand::random::<i32>();
+    let number: i32 = rand::random::<i32>();
     // ::<> is a turbofish operater to provide the DataType
 
     //let number:i32 = rand::random();
@@ -123,4 +130,116 @@ fn rustcrates(){
 
     // it will genrate i32 datatype random int
     println!("{}", number);
+}
+
+fn clArg(){
+    use std::env;
+
+    for (index, arguments) in env::args().enumerate(){
+        println!("{} is {}",index, arguments );
+    }
+
+    // access nth only
+    println!("2nd element {}", env::args().nth(2).unwrap());
+}
+
+fn fileRead(){
+    use std::fs;
+    let ErrorFound = String::from("Error found");
+    println!("file read is -> {}", match fs::read_to_string("testRead.txt") {
+        Ok(x) => x,
+        Err(_) => ErrorFound,
+    });
+
+    // or can do in debugformat printing
+    println!("file read is -> {:?}", fs::read_to_string("testRead.txt"))
+}
+
+fn fileWrite() {
+    use std::fs;
+    let text = String::from("fileWriting here only twice");
+    fs::write("testWrite.txt",text);
+}
+
+#[derive(Debug)]
+struct JLR{
+    name: String,
+    model: String,
+    cost: i32
+}
+
+// struct methods
+impl JLR {
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+    pub fn model(&self) -> &str {
+        &self.model
+    }
+    pub fn cost(&self) -> i32 {
+        self.cost
+    }
+    pub fn set_name(&mut self, name: String) {
+        self.name = name;
+    }
+    pub fn set_model(&mut self, model: String) {
+        self.model = model;
+    }
+    pub fn set_cost(&mut self, cost: i32) {
+        self.cost = cost;
+    }
+    pub fn new(name: String, model: String, cost: i32) -> Self {
+        Self { name, model, cost }
+    }
+}
+
+fn structs_here() {
+    use std::io;
+
+    //let mut new_price = String::new();
+    //io::stdin().read_line(&mut new_price);
+
+    let mut vehical = JLR::new("Range Rover".to_string(),"SV".to_string(),4500000);
+    println!("{:?}",vehical);
+
+    // struct update
+
+    let mut vehical2 = JLR {
+        name: "Defender".to_string(),
+        ..vehical
+    };
+
+    println!("{:?}",vehical2);
+    // get vehical name
+    println!("vehical name is {}",vehical2.name());
+
+    if vehical2.name() == "Defender".to_string(){
+        vehical2.set_cost(25000000);
+    }
+    println!("change cost of {} to {}",vehical2.name(),vehical2.cost());
+}
+
+
+#[derive(Debug)]
+struct shape<GenericDataType1,GenericDataType2 > {
+    width: GenericDataType1,
+    height: GenericDataType2
+}
+
+impl<GenericDataType1, GenericDataType2> shape<GenericDataType1, GenericDataType2> {
+    pub fn new(width: GenericDataType1, height: GenericDataType2) -> Self {
+        Self { width, height }
+    }
+}
+
+
+fn gneric_struct() {
+    // both width and height of same type
+    let rectangle = shape::new(30,40);
+
+    //different type
+    let square = shape::new(12.0,12);
+
+    println!("{:?}",rectangle);
+    println!("{:?}",square);
 }
